e99b8c1890eb320a599791b9ff291dd74918a1f1
Aidan Ward <wardaidan98@gmail.com>
2024-01-18 07:03:54 +0000

    Cleanup

---
+++ app/Http/Controllers/SpaceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CarPark\Space;
+
+class SpaceController extends Controller
+{
+    /**
+     * Singular parking space show action, returns the space without the ID as in
+     * this context the api consumer would already know the ID
+     *
+     * @param Space $space
+     * @return Space
+     */
+    public function show(Space $space)
+    {
+        return $space->makeHidden('id');
+    }
+}
